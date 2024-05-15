0e8ebe8eae6a5e9d9b4c3ee76a73ff5d39b4e27c
mohammad <mohammad.farhadi@bimobject.com>
2024-05-15 15:37:46 +0200

    #fix: fix issue with next.config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '38.242.213.151',
-        port: '3000',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.oldlit.yalchin.info',
-        pathname: '/api'
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'api.oldlit.yalchin.info',
+                port: '',
+                pathname: '/media/books/**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
