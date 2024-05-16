1a9bac0609bedfb0832416ef53acc01a27c4af96
mohammad <mohammad.farhadi@bimobject.com>
2024-05-16 18:57:59 +0200

    #fix: fix not showing the contact email

--- src/app/@modal/(.)books/[bookId]/page.tsx
+++ src/app/@modal/(.)books/[bookId]/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import {useParams, useRouter} from 'next/navigation'
+import {useParams} from 'next/navigation'
 import {GetBookService} from '@/services/Book.service'
 import {BookImage} from '@/components'
 import {useQuery} from '@tanstack/react-query'
 import Modal from '@/app/@modal/modal'
+import Link from "next/link";
 
 const BookModal = (props: any) => {
 
-    const { bookId } = useParams<{ bookId: string }>()
+    const {bookId} = useParams<{ bookId: string }>()
 
     const {data: book, isLoading} = useQuery({
         queryKey: ['book', bookId],
@@ -25,14 +26,19 @@ const BookModal = (props: any) => {
                     {book ? (
                         <div className='flex h-96 gap-x-8 relative'>
                             <div className='hidden h-full w-60 md:inline'>
-                                <BookImage name={book.name} image={book.images[0]} isPremium={book.isPremium} className="h-full w-full rounded-md"/>
+                                <BookImage name={book.name} image={book.images[0]} isPremium={book.isPremium}
+                                           className="h-full w-full rounded-md"/>
                             </div>
                             <div className='flex flex-1 flex-col'>
-                                <div className='flex-1'>
+                                <div className='flex-1 flex flex-col'>
                                     <p className='text-sm font-medium text-red-600'>
                                         {book.price} HUF
                                     </p>
                                     <p className='line-clamp-5 text-sm'>{book.description}</p>
+                                    <div className="mt-auto mb-4">
+                                        <small className="text-xs">Contact: <Link target="_blank" href={`mailto:${book.user.email}`}
+                                                                               className="text-red-300 underline">{book.user.email}</Link></small>
+                                    </div>
                                 </div>
                                 <div className='space-y-3 text-sm'>
                                     <button
--- src/app/books/[bookId]/page.tsx
+++ src/app/books/[bookId]/page.tsx
@@ -1,32 +1,38 @@
-import { GetBookService } from '@/services/Book.service'
+import {GetBookService} from '@/services/Book.service'
 import {BookImage, PremiumSubscription} from '@/components'
+import Link from "next/link";
 
 export const revalidate = 1;
 
-const Book = async ({ params }: { params: { bookId: number } }) => {
-  const {
-    data: book,
-  } = await GetBookService(params.bookId)
+const Book = async ({params}: { params: { bookId: number } }) => {
+    const {
+        data: book,
+    } = await GetBookService(params.bookId)
 
-  return (
-      <div className="flex justify-center mt-48">
-          <div className='flex-1 flex max-w-5xl flex-col items-center gap-8 px-4 pb-10 md:flex-row relative'>
-              <BookImage name={book.name} image={book.images[0]} isPremium={book.isPremium} className="h-full w-full max-w-xs rounded-md"/>
-              <div className='divide-y'>
-                  <div className='space-y-2 pb-8'>
-                      <h1 className='text-2xl font-bold md:text-4xl'>{book.name}</h1>
-                      <h2 className='text-xl font-bold text-red-600/70 md:text-3xl'>
-                          {book.price} HUF
-                      </h2>
-                  </div>
-                  <div className='pt-8'>
-                      <p className='text-xs md:text-sm'>{book.description}</p>
-                  </div>
-              </div>
-          </div>
-          {!book.isPremium && <PremiumSubscription/>}
-      </div>
-  )
+    return (
+        <div className="flex justify-center mt-48">
+            <div className='flex-1 flex max-w-5xl flex-col items-center gap-8 px-4 pb-10 md:flex-row relative'>
+                <BookImage name={book.name} image={book.images[0]} isPremium={book.isPremium}
+                           className="h-full w-full max-w-xs rounded-md"/>
+                <div className='divide-y'>
+                    <div className='space-y-2 pb-6'>
+                        <h1 className='text-2xl font-bold md:text-4xl'>{book.name}</h1>
+                        <h2 className='text-xl font-bold text-red-600/70 md:text-3xl'>
+                            {book.price} HUF
+                        </h2>
+                        <div className="">
+                            <small className="text-xs">Contact: <Link target="_blank" href={`mailto:${book.user.email}`}
+                                               className="text-red-300 underline">{book.user.email}</Link></small>
+                        </div>
+                    </div>
+                    <div className='pt-8'>
+                        <p className='text-xs md:text-sm'>{book.description}</p>
+                    </div>
+                </div>
+            </div>
+            {!book.isPremium && <PremiumSubscription/>}
+        </div>
+    )
 }
 
 export default Book
